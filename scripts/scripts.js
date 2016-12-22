ef469a95a183fe5c4f82c232b0d9c3c4cd70cc9e
Craig <csawayer@gmail.com>
2016-12-22 01:11:08 -0500

    I think I found the line in the JS that was causing the jumping in Edge

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,28 @@
 /*jslint browser: true*/
 /*global $, click, jQuery, alert, console*/
 
-//add scrollspy to <body>
+
+
+// add scrollspy to <body> element of HTML
 $('body').scrollspy({target: "#jsTarget", offset: 54});
 
-//add scrolling animation to all navbar links
+// add scrolling animation to all navbar links
 $('#main-nav a').on('click', function (event) {
   'use strict';
   console.log('clicked one of the nav buttons');
-
-  //prevent default nav click behavior/styling
   event.preventDefault();
-
-  //Store hash
+  //Store the id that the clicked nav button points to
   var hash = this.hash;
 
-  //Using Jquery's animate() method to add smooth page scroll
-  //the optional number- 800 - specifies the number of milliseconds it takes to scroll to the specified area (animation speed)
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top - 53
+  //800 is the optional value in milliseconds of the animation speed
+  return $('html, body').animate({
+    scrollTop: $(this.hash).offset().top - 53
   }, 800, function () {
 
-
-  //Add hash # to url when done scrolling (default click behavior)
-    window.location.hash = hash;
-  }
-     );
+    //Add hash # to url when done scrolling (default click behavior)
+    return window.history.pushState(null, null, hash);
+  });
 });
+
+//  var navOffset;
+//  navOffset = $('#jsTarget').height();
