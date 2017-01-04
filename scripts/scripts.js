204b700ddf18bb5bdc2eafd248199ed82664fbe7
Craig <csawayer@gmail.com>
2017-01-04 00:21:25 -0500

    changed JS back to how it used to be so that the tabs weren't weird in chrome

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,47 @@
 /*jslint browser: true*/
 /*global $, click, jQuery, alert, console*/
 
-// use this for adjustability:
-//  var navOffset = $('#jsTarget').height();
 
-// add scrollspy to <body> element of HTML
+//this is the code that undoes the problem in IE with the jumping
+
+//$('body').scrollspy({target: "#jsTarget", offset: 54});
+//
+//$('#main-nav a').on('click', function (event) {
+//  'use strict';
+//  console.log('clicked one of the nav buttons');
+//  event.preventDefault();
+//  //Store the id that the clicked nav button points to
+//  var hash = this.hash;
+//
+//  //800  milliseconds of the animation speed
+//  return $('html, body').animate({
+//    scrollTop: $(this.hash).offset().top - 53
+//  }, 800, function () {
+//
+//    //Add hash # to url when done scrolling (default click behavior)
+//    return window.history.pushState(null, null, hash);
+//  });
+//});
+
+
+// Add scrollspy to <body>
 $('body').scrollspy({target: "#jsTarget", offset: 54});
 
-// add scrolling animation to all navbar links
-$('#main-nav a').on('click', function (event) {
+// Add smooth scrolling on all links inside the navbar
+$("#main-nav a").on('click', function (event) {
   'use strict';
-  console.log('clicked one of the nav buttons');
   event.preventDefault();
-  //Store the id that the clicked nav button points to
+  
   var hash = this.hash;
 
-  //800 is the optional value in milliseconds of the animation speed
-  return $('html, body').animate({
-    scrollTop: $(this.hash).offset().top - 53
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top - 53
   }, 800, function () {
 
-    //Add hash # to url when done scrolling (default click behavior)
-    return window.history.pushState(null, null, hash);
+  // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
   });
 });
+
 
 
