85513c27b6c4a6c9a05e2718e5daff2515850c13
Craig Sawayer <csawayer@gmail.com>
2018-02-09 23:32:14 -0500

    cleaned up JS file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,6 @@
 //jslint browser: true
 //global $, click, jQuery, alert, console;
 
-
-//this is the code that undoes the problem in IE with the jumping
-
-var selectors = {
-    inicioTab : "#inicioTab",
-    detallesTab : "#detallesTab",
-    filosofiaTab : "#filosofiaTab",
-    contactoTab : "#contactoTab"
-};
-
 $('body').scrollspy({target: "#jsTarget", offset: 54});
 
 $('#main-nav a').on('click', function (event) {
@@ -24,48 +14,9 @@ $('#main-nav a').on('click', function (event) {
   return $('html, body').animate({
     scrollTop: $(this.hash).offset().top - 53
   }, 800, function () {
-      
-      
-      
-    if($(selectors.inicioTab).hasClass("active")){
-        $(selectors.inicioTab).css('color', '#999 !important');
-        $(selectors.inicioTab).css('color', '#999');
-        $(selectors.inicioTab).css('color', '#999');
-        $(selectors.inicioTab).css('color', '#999');
-        
-    }
+     
     //Add hash # to url when done scrolling (default click behavior)
     return window.history.pushState(null, null, hash);
       
   });
 });
-
-
-function getYearsTeaching() {
-		return document.write(new Date().getFullYear());
-}
-
-function getYearsTeachingHispanohablantes() {
-		return document.write((new Date()).getFullYear());
-}
-//// Add scrollspy to <body>
-//$('body').scrollspy({target: "#jsTarget", offset: 54});
-//
-//// Add smooth scrolling on all links inside the navbar
-//$("#main-nav a").on('click', function (event) {
-//  'use strict';
-//  event.preventDefault();
-//  
-//  var hash = this.hash;
-//
-//  $('html, body').animate({
-//    scrollTop: $(hash).offset().top - 53
-//  }, 800, function () {
-//
-//  // Add hash (#) to URL when done scrolling (default click behavior)
-//    window.location.hash = hash;
-//  });
-//});
-
-
-
